ae682a702a77b28682e61ceba9627c4d4ff3a081
Marketa Vlkova <marketa.m.vlkova@gmail.com>
2018-12-19 18:54:24 +1300

    New script producing alignments (MA) & calculating sequence identities

---
+++ Align.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+### Script for alignment and sequence identity calculation of each intergenic region among strains
+
+### example to run: bash Align.sh <path to fasta files of extracted intergenic regions> <number of which alignment length can be smaller than k12 promoter to be included>
+### e.g.: bash Align.sh output/blasted_promoters/by_promoter/ 100
+
+printf "Do you wish to align sequences? (y/n) \n"
+read ANS
+if [ "$ANS" != "${ANS#[Yy]}" ]
+then
+  for file in $1*$2*.fasta
+  do
+    NO_SUFIX="${file%.*}"
+    NAME="${NO_SUFIX#*/*/*/}"
+    echo "Aligning $NAME"
+    t_coffee $file -mode procoffee -run_name=output/alignments/$NAME &> /dev/null
+  done
+fi
+
+### delete if the file already exists
+if [ -f "output/Identities-$2.txt" ]
+then
+  rm output/Identities-$2.txt
+fi
+
+### calculate identities for aligned sequences
+echo "Calculating identities for aligned sequences"
+for align in output/alignments/*$2*.aln
+do
+  t_coffee -other_pg seq_reformat -in $align -output sim 2> /dev/null
+done >> output/Identities-$2.txt
