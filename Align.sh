a86fd32968e08861088247f1df94a5a9a3f6cbfd
Marketa Vlkova <marketa.m.vlkova@gmail.com>
2018-12-20 21:21:29 +1300

    Changes to make it compatible for integration into whole workflow script

--- Align.sh
+++ Align.sh
@@ -4,18 +4,14 @@
 ### example to run: bash Align.sh <path to fasta files of extracted intergenic regions> <number of which alignment length can be smaller than k12 promoter to be included>
 ### e.g.: bash Align.sh output/blasted_promoters/by_promoter/ 100
 
-printf "Do you wish to align sequences? (y/n) \n"
-read ANS
-if [ "$ANS" != "${ANS#[Yy]}" ]
-then
-  for file in $1*$2*.fasta
-  do
-    NO_SUFIX="${file%.*}"
-    NAME="${NO_SUFIX#*/*/*/}"
-    echo "Aligning $NAME"
-    t_coffee $file -mode procoffee -run_name=output/alignments/$NAME &> /dev/null
-  done
-fi
+### multiple sequence alignment of all found homologous intergenic regions
+for file in $1*$2*.fasta
+do
+  NO_SUFIX="${file%.*}"
+  NAME="${NO_SUFIX#*/*/*/}"
+  echo "Aligning $NAME"
+  t_coffee $file -mode procoffee -run_name=output/alignments/$NAME &> /dev/null
+done
 
 ### delete if the file already exists
 if [ -f "output/Identities-$2.txt" ]
