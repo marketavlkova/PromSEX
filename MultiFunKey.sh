a345c80a0f0c2d239f6b135b140f7364b40b974f
Marketa Vlkova <marketa.m.vlkova@gmail.com>
2020-04-09 10:51:24 +1200

    Added function joining BC codes with common gene names

--- MultiFunKey.sh
+++ MultiFunKey.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 ### extract IDs and their descriptions of ontology functions from MultiFun
-### example to run: ./MultiFunKey.sh <path to file with FultiFun IDs> <path to output file>
-### ./MultiFunKey.sh 'output/AllGenes_IDs?.txt' output/MutliFunKey.csv
+### and also join BC codes with appropriate common gene names from MultiFun
+### example to run: ./MultiFunKey.sh <path to file with FultiFun IDs> <path to output file with the key> <path to output file with join BC codes and common gene names>
+### ./MultiFunKey.sh 'output/AllGenes_IDs?.txt' output/MutliFunKey.csv output/AllGenesMFinput.csv
 
 ### loop through all file matching 1st argument
 for fileID in $1
@@ -25,6 +26,24 @@ do
     ### print ID name and its description into temporary output file
     printf "%s\t%s\n", $1, $3;
   }' > $out
+
+  ### extract the number from input file name
+  number="${tail%.*}"
+  ### join BC codes with common gene names if working with the first data files
+  if [ "${number}" == "0" ]
+  then
+    paste $fileID $fileCOM | awk 'BEGIN{
+      ### print column description
+      printf "ID,Gene\n";
+      ### set field separator as tabulator
+      FS="\t";
+    }
+    ### exclude the header for processing
+    !/Ontology/{
+      ### print ID name and its description into temporary output file
+      printf "%s,%s\n", $1, $4;
+    }' > $3
+  fi
 done
 
 ### print all temporary input files and give this output to awk
